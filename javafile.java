da4d12f3637f2584fdbb1801bc4d9bcb7e53ac8c
Dileep2186 <103827721+Dileep2186@users.noreply.github.com>
2026-06-15 22:45:15 +0530

    Update javafile.java
    from repo 15 6

---
+++ javafile.java
@@ -0,0 +1,10 @@
+public static void main(String[] args) {
+        
+        // 1. Variables & Data Types
+        String greeting = "Welcome to Java!";
+        int currentYear = 2026;
+        double piValue = 3.14159;
+        boolean isCodingFun = true;
+
+        // Printing output to the console
+        System.out.println(greeting);
